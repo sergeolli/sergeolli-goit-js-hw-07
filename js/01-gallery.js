cf1f8fbd92977a667c5ef0679fe9298bc3fdb40d
sergeolli <sergeyoleynik301087@gmail.com>
2021-12-16 11:21:17 +0200

    дз.7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems)
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener('click', onreateGalleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+     .map(({ preview, original, description }) => {
+            return `
+<div class="gallery__item">
+<a class="gallery__link" href='${original}'>
+<img class="gallery__image"
+src='${preview}'
+data-source='${original}'
+alt='${description}'/>
+</a>
+</div>`;
+      }).join('');
+}
+
+
+function onreateGalleryMarkup(e) {
+ e.preventDefault();
+  const modal = basicLightbox.create(
+`<img 
+src= ${e.target.dataset.source}
+>`);
+  modal.show()
+  
+  document.addEventListener("keydown", closeModal);
+  function closeModal(e) {
+    if (e.code === "Escape") {
+      modal.close()
+    }
+  }
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems)
+galleryContainer.innerHTML = galleryMarkup;
 
-console.log(galleryItems);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+     .map(({ preview, original, description }) => {
+       return `
+<li class="gallery__item">
+<a class="gallery__link" href='${original}'>
+<img class="gallery__image"
+src='${preview}'
+alt='${description}'/>
+</a>
+</li>`;
+       }).join('');
+};
+
+galleryContainer.addEventListener('click', onreateGalleryMarkup);
+function onreateGalleryMarkup(e) {
+  e.preventDefault();
+ const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+ });
+
+};
+
+console.log(onreateGalleryMarkup)
